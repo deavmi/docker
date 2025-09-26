1a80b4dbe57373b63f0bda1ab814f434b53e3dd0
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2025-09-26 14:36:48 +0200

    bitcoin
    - Use cookies for auth rather

--- images/bitcoin/run.sh
+++ images/bitcoin/run.sh
@@ -4,6 +4,7 @@ echo "Blocks directory: $BLOCKS_DIR"
 echo "Data directory: $DATA_DIR"
 echo "RPC username: $RPC_USERNAME"
 echo "RPC password: $RPC_PASSWORD"
+echo "Cookie file path: $RPC_COOKIE_FILE"
 
 bitcoind -blocksdir=$BLOCKS_DIR \
         -datadir=$DATA_DIR \
@@ -11,6 +12,8 @@ bitcoind -blocksdir=$BLOCKS_DIR \
         -rest \
         -rpcbind=[::]:8332 \
         -rpcallowip="::/0" \
-        -rpcuser=$RPC_USERNAME \
-        -rpcpassword=$RPC_PASSWORD \
+        -cookie_file=$RPC_COOKIE_FILE \
+        # -rpcuser=$RPC_USERNAME \
+        # -rpcpassword=$RPC_PASSWORD \
+        
         # -server \
